483b06102755ad0d89950637bc9260b729a92887
martin-markov1980 <martin.markov1980@gmail.com>
2023-09-26 10:49:54 +0100

    Compleated Adding numbers exercise.

---
+++ src/homework/AddingNumbers.java
@@ -0,0 +1,13 @@
+package homework;
+
+/*
+ * Adding two numbers
+ */
+public class AddingNumbers {
+    public static void main(String[] args) {
+        int numberOne = 17;
+        int numberTwo = 19;
+
+        System.out.println(numberOne + numberTwo);
+    }
+}
