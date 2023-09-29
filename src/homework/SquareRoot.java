424dee9949518e04392848e7c78a57ebb7501def
martin-markov1980 <martin.markov1980@gmail.com>
2023-09-29 10:40:55 +0100

    Compleated Square root of a given number to the fifth digit after decimal.

---
+++ src/homework/SquareRoot.java
@@ -0,0 +1,25 @@
+package homework;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/*
+ *   Square root of a given number to the fifth digit
+ */
+public class SquareRoot {
+    public static void main(String[] args) {
+        double userInputNumber;
+        double outputNumberResult;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter a number below:");
+        userInputNumber = scanner.nextDouble();
+
+        DecimalFormat format = new DecimalFormat("0.######");
+
+        outputNumberResult = Math.sqrt(userInputNumber);
+
+        System.out.printf("The square root number of " + format.format(userInputNumber) + " is: %,.5f", outputNumberResult);
+
+    }
+}
